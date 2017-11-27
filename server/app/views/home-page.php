b23860d01242d2e1e1ee5980b6fd02f496808eb8
Sarayut <Kopkap@Sarayuts-MacBook-Pro.local>
2017-11-27 21:01:20 +0700

    Update From Bank

--- server/app/views/home-page.php
+++ server/app/views/home-page.php
@@ -2,14 +2,18 @@
 <div class="movie-list">
     <div class="movie-list-title">
         Trends
+        <div class="pointer">
+            <i id="left" class="icon-pagi-left" /> 
+            <i id="right" class="icon-pagi-right" /> 
+        </div>
     </div>
     <div class="movie-list-body">
-        <?php for($i=0;$i<4;$i++): ?>
+        <?php for($i=0;$i<5;$i++): ?>
             <div class="movie">
                 <div class="movie-img" style="background-image:url(public/images/thor.jpg)"></div>
                 <div class="movie-title">Thor Ragnarok</div>
                 <div class="movie-detail">Imprisoned, the almighty Thor finds himself in a lethal gladiatorial contest against the Hulk, his former ally. Thor must fight for survival and race against time to prevent the all-powerful Hela from destroying his home and the Asgardian civilization.</div>
-                <div class="movie-rating">7/10 STARS</div>
+                <div class="movie-detail">7 / 10 STARS</div>
             </div>
         <?php endfor; ?>
     </div>
