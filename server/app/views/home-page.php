aba799f181cb2e13b20ebc0669f117d43700cb50
Sarayut <Kopkap@Sarayuts-MacBook-Pro.local>
2017-11-28 00:01:17 +0700

    Update Home

--- server/app/views/home-page.php
+++ server/app/views/home-page.php
@@ -11,15 +11,13 @@
         <?php for($i=0;$i<5;$i++): ?>
             <div class="movie">
                 <div class="movie-img" style="background-image:url(public/images/thor.jpg)">
-                    <?php if ($i == 0): ?>
-                        <div class="action-group">
-                            <i class="icon-add"></i>
-                            <i class="icon-play"></i>
-                            <i class="icon-share"></i>
-                        </div>
-                    <?php endif; ?>
+                    <div class="action-group">
+                        <i class="icon-add"></i>
+                        <a href="#" class="icon-play"></a>
+                        <i class="icon-share"></i>
+                    </div>
                 </div>
-                <div class="movie-title">Thor Ragnarok</div>
+                <div class="movie-title"><a href="#">Thor Ragnarok</a></div>
                 <div class="movie-detail">Imprisoned, the almighty Thor finds himself in a lethal gladiatorial contest against the Hulk, his former ally. Thor must fight for survival and race against time to prevent the all-powerful Hela from destroying his home and the Asgardian civilization.</div>
                 <div class="movie-detail">7 / 10 STARS</div>
             </div>
