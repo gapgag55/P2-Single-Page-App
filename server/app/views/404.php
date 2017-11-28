b607d119e018be33a0196a862717c1734874dff0
kongpop110758 <pinkongpop@gmail.com>
2017-11-28 14:36:32 +0700

    kong

--- server/app/views/404.php
+++ server/app/views/404.php
@@ -1,5 +1,7 @@
 <div class="not-found">
-    <h3>Oops<i class="icon-exclamation"></i></h3>
-    <p>The page you were looking for doesn’t exist.</p>
-    <a href="/">Home Page</a>
+    <div class="wraper">
+        <h3>Oops<i class="icon-exclamation"></i></h3>
+        <p>The page you were looking for doesn’t exist.</p>
+        <a href="/">Home Page</a>
+    </div>
 </div>
