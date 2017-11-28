934f7c6149fff8f095b237632e061444df155090
Sarayut <Kopkap@Sarayuts-MacBook-Pro.local>
2017-11-28 12:09:37 +0700

    update Frontend

--- server/app/views/404.php
+++ server/app/views/404.php
@@ -1,2 +1,5 @@
-404 not found
-<a href="/home"></a>
+<div class="not-found">
+    <h3>Oops<i class="icon-exclamation"></i></h3>
+    <p>The page you were looking for doesn’t exist.</p>
+    <a href="/">Home Page</a>
+</div>
