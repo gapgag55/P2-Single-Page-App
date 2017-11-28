7b5c85780c8f2ef723534a1ae8ac11552de234b4
Sarayut <Kopkap@Sarayuts-MacBook-Pro.local>
2017-11-28 14:52:07 +0700

    update 404

---
+++ public/js/api.js
@@ -0,0 +1,3 @@
+function MovieApi() {
+    this.baseurl = ''
+}
--- server/app/views/404.php
+++ server/app/views/404.php
@@ -1,5 +1,5 @@
 <div class="not-found">
-    <div class="wraper">
+    <div class="wrapper">
         <h3>Oops<i class="icon-exclamation"></i></h3>
         <p>The page you were looking for doesn’t exist.</p>
         <a href="/">Home Page</a>
