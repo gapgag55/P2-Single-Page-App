8db37b62fdc4a49677e68249d3cb5633b2c9c3ca
kongpop110758 <pinkongpop@gmail.com>
2017-11-28 11:05:52 +0700

    change fav to top

--- server/app/views/top-page.php
+++ server/app/views/top-page.php
@@ -1,3 +1,43 @@
-<h1>
-    <a href="/home">Top Movie Page </a>
-</h1>
+<div class="container top-page">
+<div class="movie-list-title">
+    Top Rating
+</div>
+<div class="row">
+<?php for($i=0;$i<2;$i++): ?>
+    <div class="col">
+        <div class="row">
+            <div class="col movie">
+                <div class="movie-img" style="background-image:url(public/images/thor.jpg)">
+                    <div class="action-group">
+                        <i class="icon-add"></i>
+                        <a href="#" class="icon-play"></a>
+                        <i class="icon-share"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col movie"><div class="movie-title"><a href="#">Thor Ragnarok</a></div>
+            <div class="movie-detail">Imprisoned, the almighty Thor finds himself in a lethal gladiatorial contest against the Hulk, his former ally. Thor must fight for survival and race against time to prevent the all-powerful Hela from destroying his home and the Asgardian civilization.</div>
+            <div class="movie-detail">7 / 10 STARS</div></div>
+        </div>
+        </div>
+
+        <?php endfor; ?>
+    <div class="w-100"></div>
+    <?php for($i=0;$i<2;$i++): ?>
+    <div class="col"><div class="row">
+            <div class="col movie">
+                <div class="movie-img" style="background-image:url(public/images/thor.jpg)">
+                <div class="action-group">
+                        <i class="icon-add"></i>
+                        <a href="#" class="icon-play"></a>
+                        <i class="icon-share"></i>
+                    </div>
+            </div>
+            </div>
+            <div class="col movie"><div class="movie-title"><a href="#">Thor Ragnarok</a></div>
+            <div class="movie-detail">Imprisoned, the almighty Thor finds himself in a lethal gladiatorial contest against the Hulk, his former ally. Thor must fight for survival and race against time to prevent the all-powerful Hela from destroying his home and the Asgardian civilization.</div>
+            <div class="movie-detail">7 / 10 STARS</div></div>
+        </div></div>
+        <?php endfor; ?>
+</div>  
+</div>
