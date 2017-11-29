c225ded5ed56add5e3f92e1bd485f6bf9566bf70
kongpop110758 <pinkongpop@gmail.com>
2017-11-30 00:25:01 +0700

    kong fav

--- server/app/views/favorites-page.php
+++ server/app/views/favorites-page.php
@@ -1,8 +1,9 @@
 <div class="container favorite-page">
 <div class="movie-list-title">
     Favorites
-    <div id="favorite" class="movie-fav"></div>
+    
 </div>
+<div id="favorite" class="movie-fav"></div>
     <script>
     var apifav = new MovieApi ();
     var numsfav = localStorage.getItem('favorites');
@@ -15,21 +16,23 @@
     })
 
     for(i=0;i<res.length;i++){
-        apifav.getById(res[i] ,function (item) {
+        let id = res[i]
+        apifav.getById(id,function (item) {
             var result = `
             
                 <div class="movie">
                     <div class="movie-img" style="background-image: url(https://image.tmdb.org/t/p/w500/${item.poster_path})">
                         <div class="action-group">
-                            <i class="icon-add"></i>
-                            <a href="/movie/" class="icon-play"></a>
+                            <i class="favorite icon-add" key=${id}></i>
+                            <a href="/movie/${id}" class="icon-play"></a>
                             <i class="icon-share"></i>
                         </div>
                     </div>  
                 </div>
             `
-            console.log(result)
-            output.append(result)         
+            
+            output.append(result)    
+            render()     
         })
     }
          
