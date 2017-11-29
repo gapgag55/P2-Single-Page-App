dd2e84df78abb71d8276362dd91c0ed5a4843346
kongpop110758 <pinkongpop@gmail.com>
2017-11-29 14:31:02 +0700

    update API

--- public/js/api.js
+++ public/js/api.js
@@ -1,5 +1,5 @@
 function MovieApi() {
-    this.baseurl = 'https://api.themoviedb.org/3/movie/'
+    this.baseurl = 'https://api.themoviedb.org/3/'
     this.key = 'aea1c02a2029aa398e2ea649ca42615a'
 }
 
@@ -19,19 +19,20 @@ MovieApi.prototype.request = async function (api, data, callback) {
 
 MovieApi.prototype.getUpComing = function (callback) {
     let data = {language: 'en-US', page: 1}
-    this.request('upcoming', data, callback)
+    this.request('movie/upcoming', data, callback)
 }
 
 MovieApi.prototype.getTopRate = function (callback) {
-    this.request('top_rated', null, callback)
+    this.request('movie/top_rated', null, callback)
 }
 
 MovieApi.prototype.getById = function (id, callback) {
-    this.request(id, null, callback)
+    this.request('movie/' + id, null, callback)
 }
 
 // MovieApi.prototype.getByMonth = function () {
 // }
 
-// MovieApi.prototype.getbySearch = function (query) {
-// }
+MovieApi.prototype.getbySearch = function (query, callback) {
+    this.request('search/movie', {query: query}, callback)
+}
