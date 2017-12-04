9711a46274476855dabfb0f7498c54e79572f8ed
tanakit_21235@hotmail.com <tanakit_21235@hotmail.com>
2017-12-05 03:41:47 +0700

    comment

--- public/js/MovieApi.js
+++ public/js/MovieApi.js
@@ -1,8 +1,12 @@
+/**
+ * Themoviedb.org API
+ */
 function MovieApi() {
     this.baseurl = 'https://api.themoviedb.org/3/'
     this.key = 'aea1c02a2029aa398e2ea649ca42615a'
 }
 
+// function for send API
 MovieApi.prototype.request = function (api, data) {
     return new Promise((resolve, reject) => {
         $.ajax({
@@ -19,30 +23,49 @@ MovieApi.prototype.request = function (api, data) {
     })
 }
 
+// use for request up-comimg data 
 MovieApi.prototype.getUpComing = function ( ) {
     return this.request(this.baseurl + 'movie/upcoming', null)
 }
 
+// use for request top-rating data 
 MovieApi.prototype.getTopRate = function (data) {
     return this.request(this.baseurl + 'movie/top_rated', data)
 }
 
+// use for request now-playing data
 MovieApi.prototype.getNowPlaying = function (data) {
     return this.request(this.baseurl + 'movie/now_playing', data)
 }
 
+// get data by using ID of movie
 MovieApi.prototype.getById = function (id) {
     return this.request(this.baseurl + 'movie/' + id, null)
 }
 
+// get data by using movie title
 MovieApi.prototype.getbySearch = function (query) {
     return this.request(this.baseurl + 'search/movie', {query})
 }
 
+// use for request credits data
 MovieApi.prototype.getCredits = function (id) {
     return this.request(this.baseurl + 'movie/'+ id +'/credits', null)
 }
 
+// use for get personal popular
+MovieApi.prototype.getPersonPopular = function (data) {
+    return this.request(this.baseurl + 'person/popular', data)
+}
+
+// use for get personal detail
+MovieApi.prototype.getPersonDetail = function (id) {
+    return this.request(this.baseurl + 'person/' + id, null)
+}
+
+/**
+ * YouTube API
+ */
 MovieApi.prototype.getYoutube = function(query) {
     return this.request(
         'https://www.googleapis.com/youtube/v3/search',
@@ -55,18 +78,16 @@ MovieApi.prototype.getYoutube = function(query) {
         })
 }
 
+/**
+ * Twitter API
+ */
 MovieApi.prototype.getComment = function(title) {
     return this.request( '/P2/twitter/' + title, null)
 }
-
+/**
+ * Spotify API
+ */
 MovieApi.prototype.getSpotify = function(title) {
     return this.request( '/P2/spotify/' + title, null)
 }
 
-MovieApi.prototype.getPersonPopular = function (data) {
-    return this.request(this.baseurl + 'person/popular', data)
-}
-
-MovieApi.prototype.getPersonDetail = function (id) {
-    return this.request(this.baseurl + 'person/' + id, null)
-}
--- public/js/routes.js
+++ public/js/routes.js
@@ -1,13 +1,15 @@
+
 function Router(app) {
     this.app = $(app)
     this.popState()
 }
-
+// load home page at first time entry
 Router.prototype.load = function () {
   let name = window.location.pathname
   this.request(name, {name})
 }
 
+// use for go to each page by using nav bar
 Router.prototype.render = function (response) {
   let self = this
   
@@ -25,22 +27,29 @@ Router.prototype.render = function (response) {
   })
 }
 
+/**
+ * send request router.php and then go to controller
+ * and then return the page(php) in view
+ */ 
 Router.prototype.request = function (url, options) {
   this.loading()
-
+  
+  // if user input "/" return homepage
   if (url == '/') {
     url = '/home'
     options.name = '/'
   }
   
+  // active hyper link in menu line 
   this.activeMenu(url);
 
+  //delay for loading screen
   setTimeout(function() {
     $.ajax({
       url: '/P2' + url
     }) 
     .done(function (response) {
-      
+      // collect state of webpage for going to previous state
       history.pushState(
         {data: response},
         options.name, 
@@ -52,6 +61,7 @@ Router.prototype.request = function (url, options) {
   }.bind(this), 900)
 }
 
+// use for back button on browser 
 Router.prototype.popState = function () {
   window.addEventListener('popstate', function(event) {
     this.render(event.state.data)
@@ -59,6 +69,7 @@ Router.prototype.popState = function () {
   }.bind(this));
 }
 
+//  use for loading screen
 Router.prototype.loading = function () {
   this.app.html(
     `<svg id="triangle" width="100px" height="100px" viewBox="-3 -4 39 39">
@@ -72,6 +83,7 @@ Router.prototype.loading = function () {
   )
 }
 
+// set active menu bar in page that is openning
 Router.prototype.activeMenu = function(url) {
 
   let a = $(`header li a[href="${url}"]`)
@@ -81,5 +93,6 @@ Router.prototype.activeMenu = function(url) {
     .removeClass()
 }
 
+// set the output area of AJAX 
 let router = new Router("#app")
 router.load()
