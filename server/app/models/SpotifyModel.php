fa63c640b4d7831fb9ce642059ec131274c7ccbc
tanakit_21235@hotmail.com <tanakit_21235@hotmail.com>
2017-12-05 22:04:25 +0700

    final

--- server/app/models/SpotifyModel.php
+++ server/app/models/SpotifyModel.php
@@ -7,13 +7,18 @@ use SpotifyWebAPI;
 class SpotifyModel {
 
     private $api;
-
+    // send API key to request the data
     public function __construct() {
         $session = new SpotifyWebAPI\Session(
             'f0d677f3dbb04911be29149f7fd8fe30',
             'c203b70a60514e41b8446b7c18a59352'
         );
 
+        /**
+         * Normally, the time for accessing spotify API is limited,
+         * so this code will generate new key every time
+         * when this function is called
+         */
         $session->requestCredentialsToken();
         $accessToken = $session->getAccessToken();
 
@@ -21,6 +26,7 @@ class SpotifyModel {
         $this->api->setAccessToken($accessToken);
     }
 
+    // return the first track of musics
     public function playlist($keyword) {
         $results = $this->api->search($keyword, 'track');
         return $results->tracks->items[0]->artists[0]->uri;
--- server/app/models/TwitterModel.php
+++ server/app/models/TwitterModel.php
@@ -8,7 +8,7 @@ use PHPInsight\Sentiment;
 class TwitterModel {
 
     private $twitter;
-
+    //connect twitter and this is the request of twitter
     public function __construct() {
         $this->twitter = new TwitterOAuth(
             'Ch8GhEOxzBMFTpP5EQn6nfdCx',
@@ -17,16 +17,20 @@ class TwitterModel {
             'gSITgARZRO9W9BNsqcsVgplEQ6vh9706OOJrfXE8vpB9K'
         );
     }
-
+    // get data by using keyword
     public function searchTweet($keyword) {
         $results = $this->twitter->get("search/tweets", [
             "q" => $keyword,
             "count" => 20
         ]);
-
+        
+        
         $results = (array) $results;
         $tweets  = $results["statuses"];
-
+        /**
+         * send each commet to analyse sentiment
+         * and store the score
+         */
         for($i = 0; $i < sizeof($tweets); $i++) {
             $score = $this->analysis( $tweets[$i]->text );
             $tweets[$i]->analysis = $score;
@@ -35,6 +39,7 @@ class TwitterModel {
         return json_encode($tweets);
     }
 
+    // anlyse the sentiment and return positive, negative, or neutral
     public function analysis($string) {
         $sentiment = new Sentiment();
         $class = $sentiment->categorise($string);
